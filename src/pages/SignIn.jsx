06b127e6a1e0001dd5a0eb79035e94c4de5c2c66
Prachi <pujariprachi17676@gmail.com>
2024-02-07 09:25:12 +0530

    create sign in, sign up, and forgot password pages and oauth component

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import { useState } from "react";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import OAuth from "../components/OAuth";
+// import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+// import { toast } from "react-toastify";
 
 export default function SignIn() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+  // const navigate = useNavigate();
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  // async function onSubmit(e) {
+  //   e.preventDefault();
+  //   try {
+  //     const auth = getAuth();
+  //     const userCredential = await signInWithEmailAndPassword(
+  //       auth,
+  //       email,
+  //       password
+  //     );
+  //     if (userCredential.user) {
+  //       navigate("/");
+  //     }
+  //   } catch (error) {
+  //     toast.error("Bad user credentials");
+  //   }
+  // }
   return (
     <section>
-        <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
-    <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
             src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1373&q=80"
@@ -13,16 +47,16 @@ export default function SignIn() {
           />
         </div>
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form >
             <input
-            //   type="email"
-            //   id="email"
-            //   value={email}
-            //   onChange={onChange}
-            //   placeholder="Email address"
+              type="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Email address"
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
             />
-            {/* <div className="relative mb-6">
+            <div className="relative mb-6">
               <input
                 type={showPassword ? "text" : "password"}
                 id="password"
@@ -73,10 +107,8 @@ export default function SignIn() {
             </div>
             <OAuth />
           </form>
-        </div> */}
-        </form>
         </div>
-    </div>
+      </div>
     </section>
-  )
+  );
 }
